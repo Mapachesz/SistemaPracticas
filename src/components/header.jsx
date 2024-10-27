6060ca9a3a999cc10a6a1fc7f8784fb898ddef95
Mapachesz <Lucastbernards@gmail.com>
2024-10-27 04:19:24 -0300

    add redirection to te login

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px', backgroundColor: '#121212', color: '#FFF' }}>
             <Typography variant="h6">Sistema de Prácticas</Typography>
-            <Link href="#" underline="hover" color="inherit">
+            <Link href="/" underline="hover" color="inherit">
                 CERRAR SESIÓN
             </Link>
         </div>
