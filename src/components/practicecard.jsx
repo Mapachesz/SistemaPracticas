496851ca802f69e622f9515c571137c013c63245
Mapachesz <Lucastbernards@gmail.com>
2024-10-27 04:16:01 -0300

    add redirection functions to the cards

--- src/components/practicecard.jsx
+++ src/components/practicecard.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function PracticeCard({ title, status, statusColor }) {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/');
+    };
     return (
-        <Card sx={{ backgroundColor: '#333', margin: '10px 0', display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
+        <Card onClick={handleClick}  sx={{ backgroundColor: '#333', margin: '10px 0', display: 'flex', justifyContent: 'space-between', padding: '10px' }}>
             <Typography variant="body1">{title}</Typography>
             <Typography variant="body2" style={{ color: statusColor }}>{status}</Typography>
         </Card>
