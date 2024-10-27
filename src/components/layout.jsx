4eedf2c2671aa93142b7260264a323346f36571a
Mapachesz <Lucastbernards@gmail.com>
2024-10-27 04:06:25 -0300

    add rute dashboard

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '../pages/home_page'
+import Dashboard from '../pages/dashboard';
 
 const Layout = () => {
   return (
     <BrowserRouter>
           <Routes>
             <Route path='/' element={<HomePage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
     </BrowserRouter>
   )
