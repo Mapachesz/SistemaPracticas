02ef7cba6dec94aef89a6c29f4803e2f7447154f
Mapachesz <Lucastbernards@gmail.com>
2024-10-27 04:25:17 -0300

    add bitacora page

---
+++ src/pages/bitacora_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BitacoraForm from '../components/bitacoraform';
+
+function BitacoraPage() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
+            <BitacoraForm />
+        </div>
+    );
+}
+
+export default BitacoraPage;
